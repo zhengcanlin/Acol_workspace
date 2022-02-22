99b6a1d41072857c9ea13e9df07e78b5ec3393f2
zhengcanlin <444130147@qq.com>
2022-02-22 22:39:35 +0800

    完成题目 30

--- leetcode/leetcode_offer.cpp
+++ leetcode/leetcode_offer.cpp
@@ -1,11 +1,63 @@
 /*
 该工程文件是剑指offer第二版的题目代码
 */
-
 #include<iostream>
 #include<vector>
 
 using namespace std;
+
+/* 简单题目 */
+
+/* 
+题号 ：No.30
+要求：定义栈的数据结构，请在该类型中实现一个能够得到栈
+      的最小元素的 min 函数在该栈中，调用 min、push 
+      及 pop 的时间复杂度都是 O(1)。
+*/
+class MinStack {
+private:
+    vector<int> buffer;
+    map<int, int> buffer_check;
+    int len;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        len = 0;
+    }
+    
+    void push(int x) {
+        this->buffer.push_back(x);
+        this->buffer_check[x]++;
+        this->len++;
+    }
+    
+    void pop() {
+        this->buffer_check[this->top()]--;
+        if(this->buffer_check[this->top()] == 0){
+            this->buffer_check.erase(this->top());
+        }
+        this->buffer.pop_back();
+        this->len--;
+    }
+    
+    int top() {
+        return this->buffer[this->len-1];
+    }
+    
+    int min() {
+        map<int,int>::iterator item = this->buffer_check.begin();
+        return item->first;
+    }
+};
+
+
+/*========================================================================*/
+
+
+/* 中等题目 */
+
+// No.
+
 
 // Definition for a binary tree node.
 struct TreeNode {
@@ -46,3 +98,9 @@ public:
         return this->build(preorder, inorder, left, right, 0);
     }
 };
+
+
+/*========================================================================*/
+
+
+/* 困难题目 */
