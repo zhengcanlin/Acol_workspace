17d9d7f03af21d4d37f19670806839674175018d
zhengcanlin <444130147@qq.com>
2022-03-06 13:28:29 +0800

    35 46 48 49

--- leetcode/leetcode_offer.cpp
+++ leetcode/leetcode_offer.cpp
@@ -200,7 +200,221 @@ public:
 
     }
 };
+
+/*
+ * @题目 : 剑指offer 35
+ * @要求 : 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，
+ *         每个节点除了有一个 next 指针指向下一个节点，还有一个 random 
+ *         指针指向链表中的任意节点或者 null。
+ * @考察类型 : 数据结构
+ */
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        map<int, Node*> index_2_address;       // 新链表中，链表序号对应的地址
+        map<Node*, int> address_2_index;       // 原链表中，地址对应的链表序号
+        Node* p = head;
+        Node* result = new Node(0);
+        Node* q = result;
+        int i = 0;
+        while(p != NULL){
+            q->next = new Node(p->val);
+            q = q->next; 
+            index_2_address[i] = q;
+            address_2_index[p] = i;
+            i++;
+            p = p->next;
+        }
+        p = head;
+        q = result->next;
+        while(p != NULL){
+            if(p->random == NULL){
+                q->random = NULL;
+            }
+            else{
+                q->random = index_2_address[address_2_index[p->random]];
+            }
+            p = p->next;
+            q = q->next;
+        }
+        return result->next;
+    }
+};
+
+/*
+ * @题目 : 剑指offer 46
+ * @要求 : 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，
+ *         1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字
+ *         可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不
+ *         同的翻译方法。
+ * @考察类型 : 递归回溯
+ * @input : 12258
+ * @output : 5
+ */
+class Solution {
+public:
+    int count = 0;
+    void check(vector<int> nums, int left, int right){
+        int temp = 0;
+        for(int i = left; i <= right; i++){
+            temp = temp * 10 + nums[i];
+        }
+        if(temp > 25){
+            return;
+        }
+        if(right == nums.size()-1){
+            count++;
+            return;
+        }
+        else{
+
+            check(nums, right+1, right+1);
+            check(nums, left, right+1);
+        }
+    }
+    int translateNum(int num) {
+        vector<int> nums;
+        if(num == 0){
+            nums.push_back(0);
+        }
+        while(num > 0){
+            nums.insert(nums.begin(), num%10);
+            num = num / 10;
+        }
+        int left = 0;
+        int right = 0;
+        check(nums, left, right);
+        return count;
+    }
+};
+
+/*
+ * @题目 : 剑指offer 48
+ * @要求 : 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+ * @考察类型 : 左右指针
+ * @input : abcabcbb
+ * @output : 3
+ */
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char, int> dictionary;
+        int left = 0;
+        int right = 0;
+        int result = 1;
+        while(right < s.length()){
+            if(dictionary[s[right]] != 0){
+                if(result < (right - left)){
+                    result = right - left;
+                }
+                dictionary[s[left]]--;
+                left++;
+            }
+            else{
+                dictionary[s[right]]++;
+                right++;
+            }
+        }
+        if((right - left) > result){
+            result = right - left;
+        }
+        return result;
+    }
+};
+
+/*
+ * @题目 : 剑指offer 49
+ * @要求 : 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+ * @考察类型 : 动态规化
+ * @input : n=10
+ * @output : 12
+ */
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int a,b,c;
+        a = b = c = 0;
+        int* dp = new int[n];
+        dp[0] = 1;
+        for(int i = 1; i < n; i++){
+            dp[i] = min(dp[a] * 2, dp[b] * 3);
+            dp[i] = min(dp[i], dp[c] * 5);
+            if(dp[i] == dp[a] * 2){
+                a++;
+            }
+            if(dp[i] == dp[b] * 3){
+                b++;
+            }
+            if(dp[i] == dp[c] * 5){
+                c++;
+            }
+        }
+        return dp[n-1];
+    }
+};
+
 /*========================================================================*/
 
 
 /* 困难题目 */
+
+/*
+ * @题目 : 剑指offer 51
+ * @要求 : 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+ * @考察类型 : 动态规化
+ * @input : [7,5,6,4]
+ * @output : 5
+ */
+class Solution {
+public:
+    int mergeSort(vector<int>& nums, vector<int>& tmp, int l, int r) {
+        if (l >= r) {
+            return 0;
+        }
+
+        int mid = (l + r) / 2;
+        int inv_count = mergeSort(nums, tmp, l, mid) + mergeSort(nums, tmp, mid + 1, r);
+        int i = l, j = mid + 1, pos = l;
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) {
+                tmp[pos] = nums[i];
+                ++i;
+                inv_count += (j - (mid + 1));
+            }
+            else {
+                tmp[pos] = nums[j];
+                ++j;
+            }
+            ++pos;
+        }
+        for (int k = i; k <= mid; ++k) {
+            tmp[pos++] = nums[k];
+            inv_count += (j - (mid + 1));
+        }
+        for (int k = j; k <= r; ++k) {
+            tmp[pos++] = nums[k];
+        }
+        copy(tmp.begin() + l, tmp.begin() + r + 1, nums.begin() + l);
+        return inv_count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> tmp(n);
+        return mergeSort(nums, tmp, 0, n - 1);
+    }
+};
